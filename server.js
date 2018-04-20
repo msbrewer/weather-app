6049da3d4c15fd04d7d48a46fabd914094a8dc8a
Michael Brewer <msbrewer@gmail.com>
2018-04-19 23:39:16 -0500

    Added post for location from user

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+    res.render('index');
+})
+
+app.post('/', function (req, res) {
+    res.render('index');
+    console.log(req.body.city);
 })
 
 app.listen(3000, function() {
